07a118813bb64c650c8127f20a58eb00bcf0fdc3
Kevin Owens <kowens@1800contacts.com>
2020-01-10 14:08:05 -0700

    Fixed bug with color overlay.

--- js/sprite.js
+++ js/sprite.js
@@ -1,9 +1,9 @@
-var Sprite = function() {
+var Sprite = function () {
 	this.animationIndex = 0;
 	this.frameImages = [];
 }
 
-Sprite.prototype.preLoadImages = function(frameSources) {
+Sprite.prototype.preLoadImages = function (frameSources) {
 	for (var i = 0; i < frameSources.length; i++) {
 		var image = new Image;
 		image.src = frameSources[i];
@@ -11,36 +11,38 @@ Sprite.prototype.preLoadImages = function(frameSources) {
 	}
 }
 
-Sprite.prototype.render = function(context, x, y) {
+Sprite.prototype.render = function (context, x, y) {
 	var image = this.frameImages[this.animationIndex];
 	context.drawImage(image, x, y);
 }
 
-Sprite.prototype.renderAsColor = function(context, x, y, color) {
+Sprite.prototype.renderAsColor = function (context, x, y, color) {
 	var image = this.frameImages[this.animationIndex];
 
-	var bufferElement = Object.assign(document.createElement("canvas"), {
-		width: image.width,
-		height: image.height
-	});
-	var bufferContext = bufferElement.getContext("2d");
-	bufferContext.drawImage(image, 0, 0);
-	bufferContext.fillStyle = color;
-	bufferContext.globalCompositeOperation = "hard-light";
-	bufferContext.fillRect(0, 0, bufferElement.width, bufferElement.height);
-	bufferContext.globalAlpha = 1;
-	bufferContext.globalCompositeOperation = "destination-in";
-	bufferContext.drawImage(image, 0, 0);
+	if (image.width > 0 && image.height > 0) {
+		var bufferElement = Object.assign(document.createElement("canvas"), {
+			width: image.width,
+			height: image.height
+		});
+		var bufferContext = bufferElement.getContext("2d");
+		bufferContext.drawImage(image, 0, 0);
+		bufferContext.fillStyle = color;
+		bufferContext.globalCompositeOperation = "hard-light";
+		bufferContext.fillRect(0, 0, bufferElement.width, bufferElement.height);
+		bufferContext.globalAlpha = 1;
+		bufferContext.globalCompositeOperation = "destination-in";
+		bufferContext.drawImage(image, 0, 0);
 
-	context.drawImage(bufferElement, x, y);
+		context.drawImage(bufferElement, x, y);
+	}
 }
 
-Sprite.prototype.renderFrame = function(context, x, y, frameIndex) {
+Sprite.prototype.renderFrame = function (context, x, y, frameIndex) {
 	var image = this.frameImages[frameIndex];
 	context.drawImage(image, x, y);
 }
 
-Sprite.prototype.update = function() {
+Sprite.prototype.update = function () {
 	if (this.hasOwnProperty('animationIndex')) {
 		this.animationIndex++;
 		if (this.animationIndex >= this.frameImages.length) {
